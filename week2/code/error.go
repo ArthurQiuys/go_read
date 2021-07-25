3f30346ff0e2c85c01c23b6c4b283e38d3fd0643
arthur <root>
2021-07-25 22:35:00 +0800

    week2 work

---
+++ week2/code/error.go
@@ -0,0 +1,31 @@
+package code
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+var NoSqlRow = errors.New("No_Sql_Row")
+
+func dao() (err error) {
+	//查询
+	//if err!=nil{
+	return errors.Wrapf(NoSqlRow, fmt.Sprintf("sql: %s error: %v", sql, err)) //sql 为语句
+	//}
+	//return
+}
+
+func biz() (err error) {
+
+	//err = dao();if errors.Is(err,NoSqlRow){
+	//	return
+	//}
+	err = dao()
+	return
+}
+
+func main() {
+	err := biz()
+	fmt.Printf("error : %+v \n", err)
+
+}
